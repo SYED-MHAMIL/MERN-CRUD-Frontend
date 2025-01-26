f759c87b26bdebbed69a729f3d7aa4c33733d5bb
SYED-MHAMIL <syedmahamil56@gmail.com>
2025-01-26 16:58:43 +0500

    'first'

--- src/lib/store.js
+++ src/lib/store.js
@@ -1,11 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import  userReducer from "@/lib/feature/userSlice.js"
-import   taskReducer from "@/lib/feature/taskSlice"
+
 export const makeStore = () => {
   return configureStore({
     reducer: {
                user : userReducer,
-               task :  taskReducer
     }
   })
 }
